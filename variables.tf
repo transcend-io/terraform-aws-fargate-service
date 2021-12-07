fcdcc339506aeb9ec2285287f12fc4cf5a28efa0
David Mattia <david@transcend.io>
2021-12-06 19:43:57 -0600

    Add ephemeral storage option (#18)
    * Add ephemeral storage option
    
    * Update required provider version
    
    * Complete version constraint
    
    * update var name

--- variables.tf
+++ variables.tf
@@ -137,3 +137,9 @@ variable capacity_provider_strategies {
   weight - Relative percentage of the total number of launched tasks that should use the specified capacity provider
   EOF
 }
+
+variable ephemeral_storage_gib {
+  description = "The total amount, in GiB, of ephemeral storage to set for the task. The minimum supported value is 20 GiB and the maximum supported value is 200 GiB."
+  type    = number
+  default = 20
+}
