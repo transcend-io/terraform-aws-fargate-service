4465af5e22994eb76528fc146edeacb457f2ccbc
Dipack <dipack@transcend.io>
2022-02-08 14:44:11 -0800

    Update description.
    Signed-off-by: Dipack <dipack@transcend.io>

--- variables.tf
+++ variables.tf
@@ -175,7 +175,7 @@ variable "capacity_provider_strategies" {
 }
 
 variable "ephemeral_storage_gib" {
-  description = "The total amount, in GiB, of ephemeral storage to set for the task. The minimum supported value is 20 GiB and the maximum supported value is 200 GiB."
+  description = "The total amount, in GiB, of ephemeral storage to set for the task. The minimum supported value is 21 GiB and the maximum supported value is 200 GiB."
   type        = number
   default     = 21
 }
